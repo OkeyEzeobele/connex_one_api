75a32c8f08c95378ec75bee954bee322febe9be1
OkeyEzeobele <chimaokeyjay@gmail.com>
2023-09-16 14:46:04 +0100

    lint fix

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -1,18 +1,18 @@
-const request = require("supertest");
-const app = require("../index");
+const request = require('supertest');
+const app = require('../index');
 
-describe("GET /time", () => {
-  it("should return 200 OK and a valid JSON object", async () => {
+describe('GET /time', () => {
+  it('should return 200 OK and a valid JSON object', async () => {
     const res = await request(app)
-      .get("/time")
-      .set("Authorization", "mysecrettoken");
+        .get('/time')
+        .set('Authorization', 'mysecrettoken');
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty("epoch");
-    expect(typeof res.body.epoch).toBe("number");
+    expect(res.body).toHaveProperty('epoch');
+    expect(typeof res.body.epoch).toBe('number');
   });
 
-  it("should return 403 Forbidden when Authorization header is missing", async () => {
-    const res = await request(app).get("/time");
+  it('should return 403 Forbidden when Authorization header is missing', async () => {
+    const res = await request(app).get('/time');
     expect(res.statusCode).toEqual(403);
   });
 });
